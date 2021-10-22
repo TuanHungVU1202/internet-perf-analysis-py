f41a05c44c60ef4e82b3d6b0c4d1b9bc087eba48
TuanHungVU1202 <tuanhung.vu122@gmail.com>
2021-10-23 01:49:48 +0300

    done Ex 3 Task 2

--- Exercise3/Ex3_Task2.py
+++ Exercise3/Ex3_Task2.py
@@ -1,6 +1,7 @@
 import csv
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 
@@ -9,7 +10,8 @@ class Ex3_Task2:
 
     def plot(self, linear_or_log):
         print("Ex3_T2")
-        raw_data, number_of_data_points = self.extract_data(linear_or_log)
+        raw_data, sorted_data, log_data, sorted_log_data, number_of_data_points = self.extract_data(linear_or_log)
+
         scatter_plot = plt.figure("I. Scatterplot")
         plt.scatter(number_of_data_points, raw_data)
         plt.title("Flow lengths in bytes and number of data points")
@@ -20,20 +22,51 @@ class Ex3_Task2:
         # numbers = [0.1, 0.5, 1, 1.5, 2, 4, 5.5, 6, 8, 9]
         plt.hist(raw_data, bins=5)
         plt.title("Flow lengths in bytes and its frequency")
-        plt.xlabel("Frequency")
-        plt.ylabel("Flow length (bytes)")
+        plt.xlabel("Flow length (bytes)")
+        plt.ylabel("Frequency")
 
         boxplot = plt.figure("III. Boxplot")
         # values = [1, 2, 5, 6, 6, 7, 7, 8, 8, 8, 9, 10, 21]
         plt.boxplot(raw_data)
         # plt.yticks(range(1, 22))
-        plt.ylabel("Value")
+        plt.ylabel("Flow length (bytes)")
+
+        # enpirical cdf
+        emp_cdf = plt.figure("IV. Empirical CDF")
+        x = sorted_data
+        y = np.arange(len(x)) / float(len(x))
+        plt.xlabel("Flow length (bytes)")
+        plt.ylabel("%")
+        plt.plot(x, y)
+
+        # Plot with LOG scale data
+        scatter_plot_log = plt.figure("I. LOG Scatterplot")
+        plt.scatter(number_of_data_points, log_data)
+        plt.title("Flow lengths in bytes and number of data points")
+        plt.xlabel("Number of Data Points")
+        plt.ylabel("Flow length (bytes)")
+
+        hist_log = plt.figure("II. LOG Histogram")
+        plt.hist(log_data, bins=10)
+        plt.title("Flow lengths in bytes and its frequency")
+        plt.xlabel("Flow length (bytes)")
+        plt.ylabel("Frequency")
+
+        boxplot_log = plt.figure("III. LOG Boxplot")
+        plt.boxplot(log_data)
+        plt.ylabel("Flow length (bytes)")
+
+        # enpirical cdf
+        emp_cdf_log = plt.figure("IV. LOG Empirical CDF")
+        x = sorted_log_data
+        y = np.arange(len(x)) / float(len(x))
+        plt.xlabel("Flow length (bytes)")
+        plt.ylabel("%")
+        plt.plot(x, y)
 
         plt.show()
 
     def extract_data(self, linear_or_log):
-        value = []
-        freq = []
         file = open("Exercise3/r-data/flows.txt", "r")
         raw_data = file.read()
         str_data = raw_data.split("\n")
@@ -41,24 +74,29 @@ class Ex3_Task2:
         # remove last blank element
         str_data.pop()
         int_data = list(map(int, str_data))
-        # traverse for all elements
-        for x in int_data:
-            # check if exists in unique_list or not
-            if x not in value:
-                value.append(x)
 
-        # sort value list
-        value.sort()
-        # traverse new value list
-        for idx, val in enumerate(value):
-            freq.insert(idx, int_data.count(val))
+        # This was intentionally to get the freq for histogram but no use
+        # value = []
+        # freq = []
+        # for x in int_data:
+        #     # check if exists in unique_list or not
+        #     if x not in value:
+        #         value.append(x)
+        #
+        # # sort value list
+        # value.sort()
+        # # traverse new value list
+        # for idx, val in enumerate(value):
+        #     freq.insert(idx, int_data.count(val))
 
-        # print(value)
-        # print(freq)
-        if linear_or_log == self.log_scale:
-            print("Getting Logarithmic scale")
-            # scale to log
-            # data =
-        return int_data, range(len(int_data))
+        # return data
+        sorted_data = np.sort(int_data)
+        log_data = list(np.log(int_data))
+        sorted_log_data = np.sort(log_data)
+        number_of_data_points = range(len(int_data))
+
+        # return raw_data, sorted_data, log_data, sorted_log_data, number_of_data_points (i.e index or number of
+        # lines in data set)
+        return int_data, sorted_data, log_data, sorted_log_data, number_of_data_points
 
     # TODO: method to scale to log scale
