b3d940bf04b03aee4cea9c0143b70d3eb69efc96
TuanHungVU1202 <tuanhung.vu122@gmail.com>
2021-10-28 21:29:26 +0300

    WIP Exercise 4 Task 1

---
+++ Exercise4/Ex4_Task1.py
@@ -0,0 +1,53 @@
+import glob
+import os
+import re
+
+
+class Ex4_Task1:
+    # col number 7, 8, 9 from crl_flow output
+    # 7: packets, 8: bytes, 9: flows
+    columns = [7, 8, 9]
+    lists_sorted_list = []
+
+    def calculate_stat(self):
+        raw_data = self.read_file()
+        self.sort_by_col(raw_data)
+
+    def read_file(self):
+        raw_data = []
+        file_list = glob.glob(os.path.join(os.getcwd(), "/Users/hungvu/Desktop/E7130/e4/out", "*.t2"))
+        for file_path in file_list:
+            with open(file_path) as f_input:
+                lines = f_input.readlines()
+                for line in lines:
+                    raw_data.append(line)
+        return raw_data
+
+    def sort_by_col(self, raw_data):
+        for col in self.columns:
+            pairs = {}
+            for line in raw_data:
+                if line == '\n' or line[0] == '#':
+                    continue
+                l = re.split('\t', line[:-1])
+                src = l[0]
+                dst = l[1]
+                # sort packets, 7th column => index 6th
+                # sort bytes, 8th column => index 7th
+                # sort flow, 9th column => index 8th
+                val = l[col - 1]
+                pair = ''
+                if src < dst:
+                    pair = src + ',' + dst
+                else:
+                    pair = dst + ',' + src
+                if pair in pairs:
+                    pairs[pair] += int(val)
+                else:
+                    pairs[pair] = int(val)
+
+            sorted_dict = sorted(pairs.items(), key=lambda x: x[1], reverse=True)
+            list_sort = list(sorted_dict)
+            self.lists_sorted_list.append(list_sort)
+
+        print(self.lists_sorted_list[2])
--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from Exercise3.Ex3_Task2 import Ex3_Task2
 from Exercise3.Ex3_Task3 import Ex3_Task3
 from Exercise3.Ex3_Task4 import Ex3_Task4
 from Exercise3.Ex3_Task5 import Ex3_Task5
+from Exercise4.Ex4_Task1 import Ex4_Task1
 
 if __name__ == '__main__':
     e3t2 = Ex3_Task2()
-    e3t2.plot()
+    # e3t2.plot()
 
     # input number of dataset available
     e3t3 = Ex3_Task3()
@@ -16,3 +17,6 @@ if __name__ == '__main__':
 
     e3t5 = Ex3_Task5()
     # e3t5.plot()
+
+    e4t1 = Ex4_Task1()
+    e4t1.calculate_stat()
