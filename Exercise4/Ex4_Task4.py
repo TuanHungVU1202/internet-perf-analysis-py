96e60ebf5f9d358bd2ebe384bc9e0cd6b51d36e5
TuanHungVU1202 <tuanhung.vu122@gmail.com>
2021-10-31 15:38:30 +0200

    Task 4.3 for ping results

--- Exercise4/Ex4_Task4.py
+++ Exercise4/Ex4_Task4.py
@@ -1,5 +1,7 @@
 import glob
 import os
+import re
+from re import search
 
 import pandas as pd
 
@@ -9,9 +11,29 @@ class Ex4_Task4:
         self.read_file()
 
     def read_file(self):
-        file_list = glob.glob(os.path.join(os.getcwd(), "/Users/hungvu/Desktop/E7130/e4/out", "*.csv"))
+        file_list = glob.glob(os.path.join(os.getcwd(), "/Users/hungvu/Desktop/E7130/e4/out", "*csv"))
         for file_path in file_list:
             print(file_path)
             data = pd.read_csv(file_path)
-            sum_rtt = data['ICMP Response Time'].sum()
-            print(sum_rtt)
+            self.cal_stats(data)
+
+    def cal_stats(self, raw_data):
+        request_str = "Echo (ping) request"
+        reply_str = "Echo (ping) reply"
+        req = 0
+        rep = 0
+
+        for value in raw_data['Info'].values:
+            if request_str in value:
+                req += 1
+            elif reply_str in value:
+                rep += 1
+
+        sum_rtt = raw_data['ICMP Response Time'].sum()
+
+        print("Sum of Response time= " + str(sum_rtt))
+        print("Average response time= " + str(sum_rtt/rep))
+        print("Request packets= " + str(req))
+        print("Reply packets= " + str(rep))
+        print("Lost ratio= " + str(1 - rep/req) + "\n")
+
