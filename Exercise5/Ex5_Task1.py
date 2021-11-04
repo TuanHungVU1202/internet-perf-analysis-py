3cef705c0278a7b7fbb1d88b2351e3d60ef61b92
TuanHungVU1202 <tuanhung.vu122@gmail.com>
2021-11-04 09:44:34 +0200

    ex5 task 1.1

--- Exercise5/Ex5_Task1.py
+++ Exercise5/Ex5_Task1.py
@@ -9,12 +9,14 @@ class Ex5_Task1:
     def plot(self):
         data = self.extract_data()
         samples = data.sample(1000)
-        parallel_coordinates(samples, 'src')
+        parallel_coordinates(samples, 'flows', cols=["src", "dst", "proto", "valid", "sport", "dport", "pkt", "bytes", "flows",
+                        "start", "end"])
+        plt.tight_layout()
         plt.show()
 
     def extract_data(self):
         file_path = '/Users/hungvu/Desktop/E7130/e5/sampling-data/flowdata.txt'
         data = pandas.read_csv(file_path, sep='\t')
-        data.columns = ["src", "dst", "proto", "port_valid", "src_port", "dst_port", "pkts", "bytes", "flows",
-                        "first_arr", "last_arr"]
-        return data
+        data.columns = ["src", "dst", "proto", "valid", "sport", "dport", "pkt", "bytes", "flows",
+                        "start", "end"]
+        return data.astype(str)
