51a8441c71cf39890dcf3ac9d8e3f64caebefcb5
TuanHungVU1202 <tuanhung.vu122@gmail.com>
2021-11-22 16:53:22 +0200

    final T1.11

--- Final/FT1_PS3.py
+++ Final/FT1_PS3.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas
 from matplotlib import pyplot as plt
 
@@ -6,19 +7,27 @@ class FT1_PS3:
         orig_data = self.extract_data()
 
         # 1.1
-        hist = plt.figure("1.1. Port distribution")
-        # plt.hist(ports, bins='auto', log=True)
-        plt.title("Port distribution")
-        plt.xlabel("Port number")
-        plt.ylabel("Freq")
+        rtt = plt.figure("1.11. Round-trip times")
+        plt.plot(orig_data['RTT_SUM'])
+        plt.title("Round-trip time")
+        plt.xlabel("Connection Number")
+        plt.ylabel("RTT (ms)")
 
-        # plt.show()
+        rtt_log = plt.figure("1.11. Round-trip times - Log Scale")
+        plt.plot(np.log(orig_data['RTT_SUM']))
+        plt.title("Round-trip time - Log Scale")
+        plt.xlabel("Connection Number")
+        plt.ylabel("RTT (ms) - Log")
+
+        plt.show()
 
 
     def extract_data(self):
         ps3 = '/Users/hungvu/Desktop/E7130/final/out/ps3.csv'
-        # col_name = ['tcp.sp', 'tcp.dp', 'udp.sp', 'udp.dp', 'len', 'time']
         data = pandas.read_csv(ps3, sep=',', skiprows=10)
-        filtered = data.loc[:, data.columns.str.startswith('RTT')]
-        print(filtered)
+        # filtered = data.loc[:, data.columns.str.startswith('RTT')]
+
+        # 1.11
+        data['RTT_SUM'] = data['RTT_samples_a2b'] + data['RTT_samples_b2a']
+        print("Round-trip times variance= " + str(data['RTT_SUM'].var()))
         return data
