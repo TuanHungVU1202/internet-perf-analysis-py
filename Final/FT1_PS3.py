bcc4cd38cb0d88fdb18f87f67f7569a284120ece
TuanHungVU1202 <tuanhung.vu122@gmail.com>
2021-11-22 17:13:01 +0200

    final T1.12

--- Final/FT1_PS3.py
+++ Final/FT1_PS3.py
@@ -6,18 +6,31 @@ class FT1_PS3:
     def plot(self):
         orig_data = self.extract_data()
 
-        # 1.1
+        # 1.11
         rtt = plt.figure("1.11. Round-trip times")
-        plt.plot(orig_data['RTT_SUM'])
+        plt.scatter(orig_data['conn_#'], orig_data['RTT_SUM'])
         plt.title("Round-trip time")
         plt.xlabel("Connection Number")
         plt.ylabel("RTT (ms)")
 
         rtt_log = plt.figure("1.11. Round-trip times - Log Scale")
-        plt.plot(np.log(orig_data['RTT_SUM']))
+        plt.scatter(orig_data['conn_#'], np.log(orig_data['RTT_SUM']))
         plt.title("Round-trip time - Log Scale")
         plt.xlabel("Connection Number")
         plt.ylabel("RTT (ms) - Log")
+
+        # 1.12
+        traffic_vol = plt.figure("1.12. Traffic volume")
+        plt.scatter(orig_data['conn_#'], orig_data['total_bytes'])
+        plt.title("Traffic volume")
+        plt.xlabel("Connection Number")
+        plt.ylabel("Traffic volume (bytes)")
+
+        traffic_vol_log = plt.figure("1.12. Traffic volume - Log Scale")
+        plt.scatter(orig_data['conn_#'], np.log(orig_data['total_bytes']))
+        plt.title("Traffic volume - Log Scale")
+        plt.xlabel("Connection Number")
+        plt.ylabel("Traffic volume (bytes) - Log")
 
         plt.show()
 
@@ -30,4 +43,8 @@ class FT1_PS3:
         # 1.11
         data['RTT_SUM'] = data['RTT_samples_a2b'] + data['RTT_samples_b2a']
         print("Round-trip times variance= " + str(data['RTT_SUM'].var()))
+
+        # 1.12
+        data['total_bytes'] = data['actual_data_bytes_a2b'] + data['actual_data_bytes_b2a']
+        print("Total traffic volumes= " + str(data['total_bytes'].sum()))
         return data
